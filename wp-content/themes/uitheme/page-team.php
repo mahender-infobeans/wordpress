3a3123f3ec2a44919a63e07fd99498e0377b5fe1
shivangi-infobeans <shivangi.verma@infobeans.com>
2017-05-02 17:29:21 +0530

    team page development

--- wp-content/themes/uitheme/page-team.php
+++ wp-content/themes/uitheme/page-team.php
@@ -11,19 +11,24 @@
 		</div>
 	</div>
 	<div class="container-fluid">
+	<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center team-slogan">
+				<p> Meet the People Who Make US Create WOW</p>
+			</div>
+		</div>
 		<div class="container">
 		<div class="row">
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-center">
-			<img src=""  alt="image-01" />
-			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-center">
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 text-center">
 				testmonial
 			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-center">
-
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 text-center">
+				testmonial
 			</div>
-			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-center">
-
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 text-center">
+				testmonial
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-4 text-center">
+				testmonial
 			</div>
 		</div>
 		</div>
