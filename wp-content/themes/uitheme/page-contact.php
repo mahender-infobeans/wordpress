d9a7fca6f264788b563a2e8b074b16a74cdf64d9
shiva.shirbhate <shiva.shirbhate@infobeans.com>
2017-05-02 12:49:32 +0530

    contact page is added

---
+++ wp-content/themes/uitheme/page-contact.php
@@ -0,0 +1,85 @@
+<?php get_header(); ?>
+<style type="text/css">
+	@font-face {
+			font-family: centuryFont;
+			src: url("font/07558_CenturyGothic.ttf");
+		}
+		body{
+			font-family: centuryFont;
+			background-color: #F7F7F7;
+		}
+
+		.bgcolor-yellow{
+			background-color: #FFFA78;
+		}
+		.bgcolor-green{
+			/*background-color: #92d17b;*/
+			background-color: #74c454;
+		}
+		.title-1{
+			color: #333;
+			/*color: #284b1b;*/
+		}
+		.portfolio-box{
+			height: 380px;
+			background-color: #ddd;
+			width: 550px;
+			margin: 10px auto;
+			display: table;
+		}
+		.color-darkgray{
+			color: #333;
+		}
+		.padding1{
+			padding: 10px;
+		}
+		.test{
+			text-align: center;
+			display: table-cell;
+			vertical-align: middle;
+		} 
+</style>
+<!-- 2nd page header -->
+<div class="aboutus-header bgcolor-green">	
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<h2 class="about-text text-trans title-1">Contact Us</h2>
+				<h1 class="we-text title-1"> Stay Connected... </h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="container">
+	<div class="row color-darkgray">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<h3 class="text-center">We would love to talk to you about any new projects</h3>
+			<h3 class="text-center">You may have or any of the services that we offer.</h3>
+		</div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="portfolio-box">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.2481684267336!2d73.91743661489342!3d18.562847137384363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c143eae1572d%3A0x6cdaaacaf2751c45!2sGiga+Space%2C+Giga+Space+IT+Park+Internal+Rd%2C+Sakore+Nagar%2C+Viman+Nagar%2C+Pune%2C+Maharashtra+411014!5e0!3m2!1sen!2sin!4v1493287566708" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
+				</div>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<div class="portfolio-box padding1">
+					<div class="test">
+						<h3><strong>E-mail:</strong></h3><p> uiteam@infobeans.com</p>
+						<h3><strong>Contact:</strong></h3><p> +91-9999(999-999)</p>
+						<h2 class="text-center">
+							<a href="JavaScript:void(0);"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+							<a href="JavaScript:void(0);"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+							<a href="JavaScript:void(0);"><i class="fa fa-tumblr-square" aria-hidden="true"></i></a>
+						</h2>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
+<?php get_footer(); ?>
