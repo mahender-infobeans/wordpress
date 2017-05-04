4ad5c46f841a5fef03d309854ad32545515b4ecf
shiva.shirbhate <shiva.shirbhate@infobeans.com>
2017-05-04 12:39:35 +0530

    contact us page is done with integration and dynamic

--- wp-content/themes/uitheme/functions/admin-menu.php
+++ wp-content/themes/uitheme/functions/admin-menu.php
@@ -13,9 +13,11 @@ function my_cool_plugin_create_menu() {
 
 function register_my_cool_plugin_settings() {
 	//register our settings
-	register_setting( 'my-cool-plugin-settings-group', 'contact_page_title' );
+	register_setting( 'my-cool-plugin-settings-group', 'emailid' );
 	register_setting( 'my-cool-plugin-settings-group', 'portfolio_page_title' );
-	register_setting( 'my-cool-plugin-settings-group', 'option_etc' );
+    register_setting( 'my-cool-plugin-settings-group', 'option_etc' );
+    register_setting( 'my-cool-plugin-settings-group', 'mobile' );
+	register_setting( 'my-cool-plugin-settings-group', 'map' );
 }
 
 function my_cool_plugin_settings_page() {
@@ -26,10 +28,11 @@ function my_cool_plugin_settings_page() {
 <form method="post" action="options.php">
     <?php settings_fields( 'my-cool-plugin-settings-group' ); ?>
     <?php do_settings_sections( 'my-cool-plugin-settings-group' ); ?>
+
     <table class="form-table">
         <tr valign="top">
-        <th scope="row">Contact Page Title</th>
-        <td><input type="text" name="contact_page_title" value="<?php echo esc_attr( get_option('contact_page_title') ); ?>" /></td>
+        <th scope="row">Email :</th>
+        <td><input type="text" name="emailid" value="<?php echo esc_attr( get_option('emailid') ); ?>" /></td>
         </tr>
          
         <tr valign="top">
@@ -40,6 +43,16 @@ function my_cool_plugin_settings_page() {
         <tr valign="top">
         <th scope="row">Options, Etc.</th>
         <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">Mobile Number</th>
+        <td><input type="text" name="mobile" value="<?php echo esc_attr( get_option('mobile') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">MAP</th>
+        <td><input type="text" name="map" value="<?php echo esc_attr( get_option('map') ); ?>" /></td>
         </tr>
     </table>
     
