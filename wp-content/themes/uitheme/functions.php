eb818ac99f5496eb56b4ae4a74658fb7330eaa9f
shivangi-infobeans <shivangi.verma@infobeans.com>
2017-05-04 11:31:37 +0530

    theme option function and some media query changes

--- wp-content/themes/uitheme/functions.php
+++ wp-content/themes/uitheme/functions.php
@@ -28,18 +28,6 @@ function homepgAnimation() {
 }
 add_action('init', 'homepgAnimation');
 
-
-
-
-
-
-
-
-
-
-
-
-
 function skematik_jquery_js(){
     wp_enqueue_script('jquery');
 }
@@ -68,4 +56,16 @@ function wpt_register_css() {
 }
 add_action( 'wp_enqueue_scripts', 'wpt_register_css' );
 
+
+// theme panel option
+function theme_settings_page(){}
+ 
+function add_theme_menu_item()
+{
+    add_menu_page("Theme Panel", "Theme Panel", "manage_options", "theme-panel", "theme_settings_page", null, 99);
+}
+
+add_action("admin_menu", "add_theme_menu_item");
+
 ?>
+
