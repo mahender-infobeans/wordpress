c1f31a670fb092f330e19c5bfeeb981c46cc4fe7
shiva.shirbhate <shiva.shirbhate@infobeans.com>
2017-05-24 13:59:38 +0530

    jquery download and added to header

--- wp-content/themes/uitheme/header.php
+++ wp-content/themes/uitheme/header.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/bootstrap.min.css">
 
 	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery.min.js"></script>
 
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
